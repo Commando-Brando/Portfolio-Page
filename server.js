81d00de619a71d31f961e79daeb79499f7aa325d
Brandon Muller <60548867+Commando-Brando@users.noreply.github.com>
2022-06-21 09:29:27 -0500

    added contact form and set up basic route for it

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/html/questionSubmit.html');;
 });
 
 app.listen(3000, () => {
